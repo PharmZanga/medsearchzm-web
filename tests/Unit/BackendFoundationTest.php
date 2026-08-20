cbb4c2b665f8143f4bd7103993265847b0647ad1
PharmZanga <zangamusakuzi@gmail.com>
2026-08-21 00:24:01 +0200

    Add backend unit test suite entry

---
+++ tests/Unit/BackendFoundationTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class BackendFoundationTest extends TestCase
+{
+    public function test_php_test_environment_is_available(): void
+    {
+        $this->assertTrue(true);
+    }
+}
